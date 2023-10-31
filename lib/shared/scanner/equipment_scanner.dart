e47c6680e55f3ad4bea93dfa21181874e1d9aaed
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2023-10-31 14:34:06 -0700

    Improved scanning performance

--- lib/shared/scanner/equipment_scanner.dart
+++ lib/shared/scanner/equipment_scanner.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
+
+import 'package:ai_barcode_scanner/ai_barcode_scanner.dart';
 import 'package:checkout/models/equipment.dart';
 import 'package:checkout/services/equipment/get_equipment.dart';
 import 'package:checkout/services/qr/parse_qr.dart';
 import 'package:checkout/shared/scanner/overlay.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_scanner/mobile_scanner.dart';
 
 Future<CheckoutEquipment?> scanEquipment(
   BuildContext context, {
@@ -18,54 +20,67 @@ Future<CheckoutEquipment?> scanEquipment(
   // Push scanner
   await Navigator.of(context).push(
     MaterialPageRoute(
-      builder: (context) => MobileScanner(
-        controller: MobileScannerController(
-            detectionSpeed: DetectionSpeed.unrestricted),
-        overlay: const ScannerOverlay(),
-        onDetect: (value) {
-          if (!canScan) {
-            return;
-          }
-          canScan = false;
-          try {
-            final int id =
-                parseQRCode(value.barcodes.first.rawValue.toString());
-
-            getEquipment(id).then((value) {
-              if (alreadyScanned.where((element) => element.id == id).isEmpty) {
-                scannedEquipment = value;
-              } else {
+      builder: (context) => Stack(children: [
+        AiBarcodeScanner(
+          //blank smallest possible size
+          bottomBar: const SizedBox(
+            height: 0,
+            width: 0,
+          ),
+          showOverlay: false,
+          canPop: false,
+          scanWindow: Rect.fromCenter(
+            center: Offset.zero,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          onScan: (value) {
+            log("HI!");
+            if (!canScan) {
+              return;
+            }
+            canScan = false;
+            try {
+              final int id = parseQRCode(value);
+              getEquipment(id).then((value) {
+                if (alreadyScanned
+                    .where((element) => element.id == id)
+                    .isEmpty) {
+                  scannedEquipment = value;
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        "Equipment already added",
+                      ),
+                    ),
+                  );
+                }
+                Navigator.of(context).pop();
+              }).onError((error, stackTrace) {
                 ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
+                  SnackBar(
                     content: Text(
-                      "Equipment already added",
+                      error.toString(),
                     ),
                   ),
                 );
-              }
-              Navigator.of(context).pop();
-            }).onError((error, stackTrace) {
+                Navigator.of(context).pop();
+              });
+            } catch (e) {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
-                    error.toString(),
+                    "Failed to add equipment: $e",
                   ),
                 ),
               );
               Navigator.of(context).pop();
-            });
-          } catch (e) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                  "Failed to add equipment: $e",
-                ),
-              ),
-            );
-            Navigator.of(context).pop();
-          }
-        },
-      ),
+            }
+          },
+        ),
+        const ScannerOverlay(),
+      ]),
     ),
   );
   return scannedEquipment;
