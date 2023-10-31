bd001097465743a38822077fdcc60fb9454edb84
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2023-10-31 13:41:57 -0700

    Simplified user registration process

--- lib/services/user/register_user.dart
+++ lib/services/user/register_user.dart
@@ -1,30 +1,20 @@
 import 'package:checkout/globals.dart' as globals;
 
 Future<void> registerUser() async {
-  // Check if user exists
-  try {
-    final response = await globals.supabase
-        .from('users')
-        .select()
-        .eq('id', globals.supabase.auth.currentUser!.id);
-    if (response != null) {
-      globals.supabase.from('users').update({
-        'id': globals.supabase.auth.currentUser!.id,
-        'profile_picture':
-            globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
-      });
-    }
-  } catch (ignore) {
-    // Register user
-    await globals.supabase.from('users').insert(
-      {
-        'id': globals.supabase.auth.currentUser!.id,
-        'name': globals.supabase.auth.currentUser!.userMetadata?['name'],
-        'email': globals.supabase.auth.currentUser!.email,
-        'profile_picture':
-            globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
-        'admin': false,
-      },
-    );
-  }
+  await globals.supabase.from('users').insert(
+    {
+      'id': globals.supabase.auth.currentUser!.id,
+      'name': globals.supabase.auth.currentUser!.userMetadata?['name'],
+      'email': globals.supabase.auth.currentUser!.email,
+      'profile_picture':
+          globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
+      'admin': false,
+    },
+  ).onError((error, stackTrace) async {
+    await globals.supabase.from('users').update({
+      'id': globals.supabase.auth.currentUser!.id,
+      'profile_picture':
+          globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
+    });
+  });
 }
