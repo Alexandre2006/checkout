c73b9f21c6759ed36bc058b9fbe54edc98d73ac0
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2023-10-31 13:45:25 -0700

    Added some error handling :)

--- lib/services/user/register_user.dart
+++ lib/services/user/register_user.dart
@@ -1,20 +1,24 @@
 import 'package:checkout/globals.dart' as globals;
 
 Future<void> registerUser() async {
-  await globals.supabase.from('users').insert(
-    {
-      'id': globals.supabase.auth.currentUser!.id,
-      'name': globals.supabase.auth.currentUser!.userMetadata?['name'],
-      'email': globals.supabase.auth.currentUser!.email,
-      'profile_picture':
-          globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
-      'admin': false,
-    },
-  ).onError((error, stackTrace) async {
-    await globals.supabase.from('users').update({
-      'id': globals.supabase.auth.currentUser!.id,
-      'profile_picture':
-          globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
+  try {
+    await globals.supabase.from('users').insert(
+      {
+        'id': globals.supabase.auth.currentUser!.id,
+        'name': globals.supabase.auth.currentUser!.userMetadata?['name'],
+        'email': globals.supabase.auth.currentUser!.email,
+        'profile_picture':
+            globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
+        'admin': false,
+      },
+    ).onError((error, stackTrace) async {
+      await globals.supabase.from('users').update({
+        'id': globals.supabase.auth.currentUser!.id,
+        'profile_picture':
+            globals.supabase.auth.currentUser!.userMetadata!['avatar_url'],
+      });
     });
-  });
+  } catch (ignore) {
+    // User already exists
+  }
 }
